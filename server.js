768a1dff80f72ebffbfc01143186b6486a030403
hs242424 <79582976+hs242424@users.noreply.github.com>
2022-02-27 10:29:14 -0500

    yes

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
+const e = require('express');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var sqlite3 = require('sqlite3').verbose();
+
+// Loads the database
+let db = new sqlite3.Database('./database/data.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err){
+        console.log(err);
+    }
+});
+//database is currently empty
 
 const port = 8080;
 
 app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res){
     res.sendFile(__dirname + "/index.html");
  });
@@ -14,6 +25,7 @@ app.get('/', function(req, res){
 app.get('/index.js', function(req, res){
     res.sendFile(__dirname + "/index.js");
 });
+
 io.on('connection', function(socket) {
     socket.emit('message', 'Hello World')
 });
